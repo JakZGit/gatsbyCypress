de6da8816eeaa598dfc18b9a3627382b69daf1f5
Jack Zhang <jackzhng528@gmail.com>
2021-06-09 15:46:45 -0400

    update

--- test.js
+++ test.js
@@ -2,13 +2,15 @@ const fetch = require('node-fetch')
 async function testing() {
 
 	const response = await fetch('https://uipath.com/fsfdsf');
-	if(response.status === 404)
+	if(response.status === 404) {
 		throw new Error('This page should not be 404')
+		process.exit(1);
+	}
 }
 
-testing().catch((err) => {
-	// throw err
-	process.exit(1);
-	console.log("testing")
-});
+testing()
+// testing().catch((err) => {
+// 	throw err
+// 	process.exit(1);
+// });
 
