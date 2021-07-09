8e5f054d12ec586b06156a9c5b8abe2859aac5bf
Jack Zhang <jackzhng528@gmail.com>
2021-07-09 11:39:35 -0400

    test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
-it("shows the Hello page", () => {
+it("shows the Hello fail", () => {
   cy.visit("/").contains("Hello worjld!")
 })
+
+it("shows the Hello success", () => {
+  cy.visit("/").contains("Hello world!")
+})
+
