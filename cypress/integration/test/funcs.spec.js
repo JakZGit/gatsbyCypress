30654cbdf24b6ea9c64053f55c9a344fe791b779
JakZGit <jackzhng528@gmail.com>
2021-09-09 11:19:56 -0400

    fix ref

--- cypress/integration/test/funcs.spec.js
+++ cypress/integration/test/funcs.spec.js
@@ -1,7 +1,7 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
-import { add } from '../../src/funcs'
+import { add } from '../../../src/funcs'
 
 it("adds two numbers", () => {
   expect(add(1,2)).to.equal(3)
