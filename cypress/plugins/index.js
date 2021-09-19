c5c821084e8eda49b152132833b4e9d4227dfafd
JakZGit <jackzhng528@gmail.com>
2021-09-19 14:58:51 -0400

    revert

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,6 @@
 module.exports = (on, config) => {
-  if(!process.env.IGNORE_COVERAGE) require("@cypress/code-coverage/task")(on, config)
+  // if(!process.env.IGNORE_COVERAGE) 
+  require("@cypress/code-coverage/task")(on, config)
   // console.log("process.env", process.env.DISABLE_COVERAGE)
 
   // add other tasks to be registered here
